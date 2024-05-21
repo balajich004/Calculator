6c1b13f3ce8d5262378d26000692bf5526d06888
balajich004 <balajichennupati123@gmail.com>
2024-05-21 13:54:34 +0530

    successfully completed all basic functionalities of calculator need to do some more

--- scripts/get.js
+++ scripts/get.js
@@ -1,12 +1,30 @@
+let input=document.querySelector("#input");
+let first,second,result=0,operator;
+let array=[],step=0;
+let secondarray=[]
 function getNumber(num){
-    console.log(num);
+    if(step===0 || step===1){
+    array.push(num);
+    first=Number(array.join(""));
+    input.value=first;
+    step=1;
+    }
+    else if(step===2){
+        secondarray.push(num);
+        second=Number(secondarray.join(""));
+        input.value=second;
+    }
 }
 function getoperator(op){
-    console.log(op);
+    step=2;
+    operator=op;
 }
 function clearinput(){
-    console.log("cleared");    
+    input.value="";
+    first=null;
+    second=null;
+    operator=null;
+    array=[];
+    secondarray=[];  
+    step=0; 
 }
-let calculate=()=>{
-    console.log("calculated");
-};
--- scripts/script.js
+++ scripts/script.js
@@ -1,28 +1,23 @@
-let first;
-let second;
-let operator;
-
-const add=(a,b)=>{
-    return a+b;
-}
-const sub=(a,b)=>{
-    return a-b;
-}
-const mul=(a,b)=>{
-    return a*b;
-}
-const div=(a,b)=>{
-    return a/b;
-}
-const mod=(a,b)=>{
-    return a%b;
-}
-function operate(a,b,operator){
-    switch(operator){
-        case '+':add(a,b);break;
-        case '-':sub(a,b);break;
-        case '*':mul(a,b);break;
-        case '/':div(a,b);break;
-        case '%':mod(a,b);break;
+const calculate=()=>{
+    if(operator==='+'){
+        result=first+second;
+        input.value=result;
     }
-}
+    else if(operator==='-'){
+        result=first-second;
+        input.value=result;
+    }
+    else if(operator==='*'){
+        result=first*second;
+        input.value=result;
+    }
+    else if(operator==='/'){
+        result=first/second;
+        input.value=result;
+    }
+    else if(operator==='%'){
+        result=first%second;
+        input.value=result;
+    }
+    
+};
