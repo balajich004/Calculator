fe4311fc9d2fcc03bfe335aede09cfe845bd5283
balajich004 <balajichennupati123@gmail.com>
2024-05-21 12:30:00 +0530

    Rethought my js algorithm for the calculator

---
+++ scripts/get.js
@@ -0,0 +1,12 @@
+function getNumber(num){
+    console.log(num);
+}
+function getoperator(op){
+    console.log(op);
+}
+function clearinput(){
+    console.log("cleared");    
+}
+let calculate=()=>{
+    console.log("calculated");
+};
